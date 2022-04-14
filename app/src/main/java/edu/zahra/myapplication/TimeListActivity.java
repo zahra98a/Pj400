148a4b01db01fbdbb86c6970b9c5b1db6bac0787
Zahra Al Bulushi <47219252+zahra98a@users.noreply.github.com>
2022-04-14 22:59:23 +0100

    implement time slot

--- app/src/main/java/edu/zahra/myapplication/TimeListActivity.java
+++ app/src/main/java/edu/zahra/myapplication/TimeListActivity.java
@@ -1,25 +1,45 @@
 package edu.zahra.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.ArrayList;
 
 public class TimeListActivity extends AppCompatActivity {
 
     ListView TimeList;
-
+    CalendarView calendarView;
+    TextView choosenDate;
+    TextView choosenTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_list);
 
         TimeList = findViewById(R.id.lvTime);
+        calendarView = findViewById(R.id.calendarView);
+        choosenDate = findViewById(R.id.tvChoosenDate);
+        choosenTime = findViewById(R.id.tvChoosenTime);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+                String dateChoosen = (i1 + 1) + "/" + i2 + "/" + i;
+                choosenDate.setText(dateChoosen);
+            }
+        });
 
         ArrayList<String> arrayList = new ArrayList<>();
 
@@ -34,10 +54,26 @@ public class TimeListActivity extends AppCompatActivity {
         arrayList.add("07:00 - 08:00");
         arrayList.add("08:00 - 09:00");
 
-
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
 
         TimeList.setAdapter(arrayAdapter);
+
+
+        //on click select the playground
+        TimeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //DatabaseReference itemRef = adapter.getRef(position);
+              //  int selected = position;
+
+              //  MyClass selItem =  p.getItem(position);
+                String selectedFromList =(String) (TimeList.getItemAtPosition(position));
+
+                choosenTime.setText(selectedFromList);
+
+            }
+        });
 
     }
 
